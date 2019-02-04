b30291ae8fc749f34d07b20da3ebb57260454d02
demisgomes <demismg72@gmail.com>
2019-02-04 12:03:59 -0200

    refactoring of methods

--- src/main/kotlin/service/GenericService.kt
+++ src/main/kotlin/service/GenericService.kt
@@ -3,10 +3,12 @@ package service
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.core.isSuccessful
-import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
 import exception.UserException
 
 class GenericService{
@@ -18,71 +20,49 @@ class GenericService{
     inline fun <reified T> delete(id:Int): GenericResponse<T>{
         val (_,response,result) =
                 Fuel.delete("$postUrl/$id").response()
-        if(!response.isSuccessful){
-            val errorMessage=ObjectMapper().readTree(response.data)
-            throw UserException("error on DELETE\nStatus: ${response.statusCode}\nError: $errorMessage")
-        }
-        val responseData=objectMapper.readValue(result.get(), T::class.java)
-        return GenericResponse(response.statusCode, responseData, response.responseMessage)
+        return responseHandler(response,result,"DELETE")
     }
 
 
     inline fun <reified T> put(id:Int, requestObject: T): GenericResponse<T>{
         val (_,response,result) =
                 Fuel.put("$postUrl/$id").jsonBody(ObjectMapper().writeValueAsString(requestObject)).response()
-        if(!response.isSuccessful){
-            val errorMessage=ObjectMapper().readTree(response.data)
-            throw UserException("error on PUT\nStatus: ${response.statusCode}\nError: $errorMessage")
-        }
-        val responseData=objectMapper.readValue(result.get(), T::class.java)
-        return GenericResponse(response.statusCode, responseData, response.responseMessage)
+        return responseHandler(response,result,"PUT")
     }
 
 
     inline fun <reified T, reified S> post(requestObject: T): GenericResponse<S>{
         val (_,response,result) =
                 Fuel.post(postUrl).jsonBody(ObjectMapper().writeValueAsString(requestObject)).response()
-        if(!response.isSuccessful){
-            val errorMessage=ObjectMapper().readTree(response.data)
-            throw UserException("error on POST\nStatus: ${response.statusCode}\nError: $errorMessage")
-        }
-        val responseData=objectMapper.readValue(result.get(), S::class.java)
-        return GenericResponse(response.statusCode, responseData, response.responseMessage)
+        return responseHandler(response,result,"POST")
     }
 
     inline fun <reified T, reified S> post(requestObject: T, headers:Map<String,String>): GenericResponse<S>{
         val (_,response,result) =
                 Fuel.post(loginUrl).header(headers).jsonBody(ObjectMapper().writeValueAsString(requestObject)).response()
-        if(!response.isSuccessful){
-            val errorMessage=ObjectMapper().readTree(response.data)
-            throw UserException("error on POST\nStatus: ${response.statusCode}\nError: $errorMessage")
-        }
-        val responseData=objectMapper.readValue(result.get(), S::class.java)
-        return GenericResponse(response.statusCode, responseData, response.responseMessage)
+        return responseHandler(response,result,"POST")
     }
 
 
     inline fun <reified T, reified S> post2(requestObject: T): GenericResponse<S>{
         val (_,response,result) =
                 postUrl.httpPost().jsonBody(ObjectMapper().writeValueAsString(requestObject)).response()
-        if(!response.isSuccessful){
-            val errorMessage=ObjectMapper().readTree(response.data)
-            throw UserException("error on POST\nStatus: ${response.statusCode}\nError: $errorMessage")
-        }
-        val responseData=objectMapper.readValue(result.get(), S::class.java)
-        return GenericResponse(response.statusCode, responseData, response.responseMessage)
+        return responseHandler(response,result,"POST")
     }
 
     inline fun <reified T> get():GenericResponse<T>{
         val (_,response,result) =
                 Fuel.get(getUrl).response()
-        //val (_,response,result) = getUrl.httpGet().response()
+        return responseHandler(response,result,"GET")
+    }
+
+    inline fun<reified T>  responseHandler(response: Response, result:Result<ByteArray,FuelError>,method:String): GenericResponse<T> {
         if(!response.isSuccessful){
             val errorMessage=ObjectMapper().readTree(response.data)
-            throw UserException("error on GET\nStatus: ${response.statusCode}\nError: $errorMessage")
+            throw UserException("error on $method\nStatus: ${response.statusCode}\nError: $errorMessage")
         }
         val responseData=objectMapper.readValue(result.get(), T::class.java)
         return GenericResponse(response.statusCode, responseData, response.responseMessage)
     }
-
 }
+
